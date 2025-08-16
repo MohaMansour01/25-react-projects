1d1260a40aa2fbe750613024dca2bc0e5e5d93a2
moha_01 <mm7.mansour@gmail.com>
2025-08-16 05:43:21 -0700

    completed random color selection project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import Accordian from './components/Accordion'
+import RandomColor from './components/RandomColorGenerator'
 
 function App() {
 
   return (
       <div className='App'>
         {/* Accordian component */}
-        <Accordian />
+        {/* <Accordian /> */}
+        {/* Random Color Selector */}
+        <RandomColor />
       </div>
   )
 }
